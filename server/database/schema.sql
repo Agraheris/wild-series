8eb74335ac861895109409ad273e33420d0cc777
clement Vigouroux <clement.vigouroux@gmail.com>
2024-08-05 16:31:42 +0200

    :sparkles: add table in database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,18 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+CREATE TABLE category (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(100)
+);
+
+CREATE TABLE program (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(250) NOT NULL,
+  synopsis TEXT NOT NULL,
+  poster VARCHAR(250) NOT NULL,
+  country VARCHAR(250) NOT NULL,
+  YEAR INT NOT NULL,
+  caterogy_id INT NOT NULL
+);
